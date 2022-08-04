abf9c4bac8c746cce2589b048422a21999b6bb3f
Talita <talitahard20@gmail.com>
2022-08-04 20:33:04 -0300

    first commit

---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,32 @@
+package br.com.dio.model;
+
+public class Gato {
+    private String nome;
+    private String cor;
+    private Integer idade;
+
+    public Gato (){}
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome='" + nome + '\'' +
+                ", cor='" + cor + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+
+
+}
